11dcd2624d852cffd9a5390a27509f1d19acaebf
waelby99 <waelbenyoussef19991@gmail.com>
2023-09-17 20:25:17 +0100

    updates search

--- src/app/caution/caution.component.ts
+++ src/app/caution/caution.component.ts
@@ -1,4 +1,4 @@
-import { Component,OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Caution } from '../models/caution.model';
 import { Router } from '@angular/router';
 import { CautionService } from '../services/caution.service';
@@ -8,24 +8,80 @@ import { CautionService } from '../services/caution.service';
   templateUrl: './caution.component.html',
   styleUrls: ['./caution.component.css']
 })
-export class CautionComponent implements OnInit{
-  ToShow:Caution[]=[];
-  constructor(private cautionService:CautionService,private router:Router){}
+export class CautionComponent implements OnInit {
+  ToShow: Caution[] = [];
+  //searchQuery: string = '';
+  searchMode: string = 'reference'; // Mode de recherche par défaut
+  referenceQuery: string = ''; // Valeur de recherche par référence
+  etatQuery: string = ''; // Valeur de recherche par état
+  startDate: Date = new Date(); // Date de début pour la recherche par date de caution
+  endDate: Date = new Date();
+  
+  
+  constructor(private cautionService: CautionService, private router: Router) {}
 
   ngOnInit(): void {
-    this.cautionService.getAll().subscribe(
-      data=>{
-        this.ToShow=data;
-      }
-    )
+    this.cautionService.getAll().subscribe((data) => {
+      this.ToShow = data;
+    });
   }
-  detailCaut(idCaution:any){
-    this.router.navigate(['cautions/details/'+idCaution]);
+
+  
+
+  detailCaut(idCaution: any) {
+    this.router.navigate(['cautions/details/' + idCaution]);
   }
-  addcaut(){
+
+  addcaut() {
     this.router.navigate(['addcaution']);
   }
-  edit(idCaution:any){
-    this.router.navigate(['cautions/modif/'+idCaution]);
+
+  edit(idCaution: any) {
+    this.router.navigate(['cautions/modif/' + idCaution]);
   }
+
+  // Function to filter the ToShow array based on the search query
+/*filterCautions() {
+  if (!this.searchQuery) {
+    // If the search query is empty, load all cautions
+    this.loadCautions();
+    return;
+  }
+
+  // Filter cautions based on the search query
+  this.ToShow = this.ToShow.filter((caution) => {
+    // Check if caution and caution.reference are defined before using them
+    if (caution && caution.reference) {
+      return (caution.reference.toLowerCase().includes(this.searchQuery.toLowerCase())
+      );
+    } else {
+      // Handle the case where caution or caution.reference is undefined
+      return false;
+    }
+  });
+}*/
+search() {
+  // Réinitialisez la liste ToShow en fonction des critères de recherche
+  if (this.searchMode === 'reference') {
+    this.ToShow = this.ToShow.filter((caution:any) =>
+      caution.reference.includes(this.referenceQuery)
+    );
+  } else if (this.searchMode === 'etat') {
+    this.ToShow = this.ToShow.filter((caution) =>
+      caution.etat === this.etatQuery
+    );
+  } else if (this.searchMode === 'date') {
+    // Effectuez la recherche par date de caution en utilisant this.startDate et this.endDate
+    const startDate = new Date(this.startDate);
+    const endDate = new Date(this.endDate);
+
+    // Effectuez la recherche par date de caution
+    this.ToShow = this.ToShow.filter((caution:any) => {
+      const cautionDate = new Date(caution.datecaut);
+      return cautionDate >= startDate && cautionDate <= endDate;
+    });
+  }
+}
+
+
 }
